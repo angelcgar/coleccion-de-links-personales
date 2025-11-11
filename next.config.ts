1d9e3a5349057f9829ce2b9ec8bdcda3a8ca9070
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-11-11 08:54:32 -0600

    fix: ignorar archivos LICENSE para turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        "*.LICENSE": { loaders: ["ignore"] },
+      },
+    },
+  },
 };
 
 export default nextConfig;
